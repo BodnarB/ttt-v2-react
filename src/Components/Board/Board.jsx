74ccb302c2267b854ab7abd4d41b2f67b9bb37a3
Boglarka Bodnar <bodnarbog94@gmail.com>
2022-12-11 12:13:46 +0100

    update states

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -2,57 +2,70 @@ import React, { useState, useEffect } from 'react'
 import Info from '../Info/Info'
 import Row from '../Row/Row'
 import '../Board/Board.css'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Board() {
     const winPostions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    const [nextPlayer, setNextPlayer] = useState('x')
-    const [board, setBoard] = useState(Array(9).fill(''))
-    const [xPositions, setXpositions] = useState([])
-    const [OPositions, setOpositions] = useState([])
-    const [BO, setBO] = useState(3)  // This will be used later
-    const [winner, setWinner] = useState('')
-    const [results, setResults] = useState([])
-    const [finalWinner, setFinalWinner] = useState('')
-    const [disabledButton, setDisabledButton] = useState(true)
-    const [cellStyle, setCellStyle] = useState(Array(9).fill(''))
+    const [gameStates, setGameStates] = useState({
+        nextPlayer: 'x',
+        board: Array(9).fill(''),
+        xPositions: [],
+        oPositions: [],
+        BO: 3,
+        winner: '',
+        results: [],
+        finalWinner: '',
+        disabledButton: true,
+        cellStyle: Array(9).fill('')
+    })
 
     useEffect(() => {
-        if (winner !== '' && winner !== '-') {
-            setResults([...results, winner])
+        if (gameStates.winner !== '') {
+            setGameStates({
+                ...gameStates,
+                disabledButton: false,
+                results: [...gameStates.results, gameStates.winner]
+            })
         }
-        if (winner !== '') {
-            setDisabledButton(false)
-        }
-    }, [winner])
+    }, [gameStates.winner])
 
     useEffect(() => {
-        if (results.length >= 2) {
-            if (results.filter(res => res === 'x').length === 2) {
-                setFinalWinner('x')
-            }
-            else if (results.filter(res => res === 'o').length === 2) {
-                setFinalWinner('o')
+        if (gameStates.results.length >= 2) {
+            if (gameStates.results.filter(res => res === 'x').length === 2) {
+                setGameStates({
+                    ...gameStates,
+                    finalWinner: 'x'
+                })
+            } else if (gameStates.results.filter(res => res === 'o').length === 2) {
+                setGameStates({
+                    ...gameStates,
+                    finalWinner: 'o'
+                })
             }
         }
-    }, [results])
+    }, [gameStates.results])
 
     function checkPlayer(player) {
         if (player.length >= 3) {
             winPostions.forEach((item, index) => {
                 let match = player.filter(element => winPostions[index].includes(element))
                 if (match.length === 3) {
-                    if (player === xPositions) {
-                        setWinner('x')
+                    let newWinner = ''
+                    if (player === gameStates.xPositions) {
+                        newWinner = 'x'
                     }
-                    else if (player === OPositions) {
-                        setWinner('o')
+                    else if (player === gameStates.oPositions) {
+                        newWinner = 'o'
                     }
-                    let newStyle = cellStyle
+                    let newStyle = gameStates.cellStyle
                     match.forEach((item) => {
                         newStyle[item] = 'win-bg'
                     })
-                    setCellStyle(newStyle)
+                    setGameStates({
+                        ...gameStates,
+                        winner: newWinner,
+                        cellStyle: newStyle
+                    })
                 }
             })
         }
@@ -60,59 +73,76 @@ export default function Board() {
 
     useEffect(() => {
         function checkWinner() {
-            if (finalWinner === '') {
-                if (!board.includes('')) {
-                    setWinner('-')
+            if (gameStates.finalWinner === '') {
+                if (!gameStates.board.includes('')) {
+                    setGameStates({
+                        ...gameStates,
+                        winner: '-'
+                    })
                 }
-                checkPlayer(xPositions)
-                checkPlayer(OPositions)
+                checkPlayer(gameStates.xPositions)
+                checkPlayer(gameStates.oPositions)
             }
         }
         checkWinner()
-    }, [board])
+    }, [gameStates.board])
 
     useEffect(() => {
-        if (finalWinner !== '') {
-            setNextPlayer('-')
-            setDisabledButton(true)
+        if (gameStates.finalWinner !== '') {
+            setGameStates({
+                ...gameStates,
+                nextPlayer: '-',
+                disabledButton: true
+            })
         }
-    }, [finalWinner])
+    }, [gameStates.finalWinner])
 
     function handleNewGame() {
-        setCellStyle(Array(9).fill(''))
-        setBoard(Array(9).fill(''))
-        setXpositions([])
-        setOpositions([])
-        setResults([])
-        setWinner('')
-        setNextPlayer('x')
-        setFinalWinner('')
+        setGameStates({
+            ...gameStates,
+            cellStyle: Array(9).fill(''),
+            board: Array(9).fill(''),
+            xPositions: [],
+            oPositions: [],
+            results: [],
+            winner: '',
+            nextPlayer: 'x',
+            finalWinner: ''
+        })
     }
 
     function handleNextGame() {
-        setCellStyle(Array(9).fill(''))
-        setBoard(Array(9).fill(''))
-        setDisabledButton(true)
-        setXpositions([])
-        setOpositions([])
-        setWinner('')
+        setGameStates({
+            ...gameStates,
+            cellStyle: Array(9).fill(''),
+            board: Array(9).fill(''),
+            disabledButton: true,
+            xPositions: [],
+            oPositions: [],
+            winner: ''
+        })
     }
 
     function handleClick(e) {
-        if (winner === '') {
+        if (gameStates.winner === '') {
             let targetCell = Number(e.target.dataset.id)
-            if (board[targetCell] === '') {
-                let newBoard = [...board]
-                newBoard[targetCell] = nextPlayer
-                if (nextPlayer === 'o') {
-                    setOpositions([...OPositions, targetCell])
-                    setNextPlayer('x')
+            if (gameStates.board[targetCell] === '') {
+                let newBoard = [...gameStates.board]
+                newBoard[targetCell] = gameStates.nextPlayer
+                let newXPositions = [...gameStates.xPositions]
+                let newOPositions = [...gameStates.oPositions]
+                if (gameStates.nextPlayer === 'o') {
+                    newOPositions.push(targetCell)
+                } else {
+                    newXPositions.push(targetCell)
                 }
-                else {
-                    setXpositions([...xPositions, targetCell])
-                    setNextPlayer('o')
-                }
-                setBoard(newBoard)
+                setGameStates({
+                    ...gameStates,
+                    board: newBoard,
+                    xPositions: newXPositions,
+                    oPositions: newOPositions,
+                    nextPlayer: gameStates.nextPlayer === 'o' ? 'x' : 'o'
+                })
             }
         }
     }
@@ -120,20 +150,20 @@ export default function Board() {
     const cells = [0, 1, 2].map(rowIndex =>
         [0, 1, 2].map(colIndex => ({
             cellID: rowIndex * 3 + colIndex,
-            cellContent: board[rowIndex * 3 + colIndex],
-            cellStyle: cellStyle[rowIndex * 3 + colIndex]
+            cellContent: gameStates.board[rowIndex * 3 + colIndex],
+            cellStyle: gameStates.cellStyle[rowIndex * 3 + colIndex]
         }))
     )
 
     return (
         <>
             <Info
-                nextPlayer={nextPlayer}
+                nextPlayer={gameStates.nextPlayer}
                 newGame={handleNewGame}
                 nextGame={handleNextGame}
-                games={BO}
-                disabledNext={disabledButton}
-                results={results.map((item) => <p key={uuidv4()}>{item}</p>)}
+                games={gameStates.BO}
+                disabledNext={gameStates.disabledButton}
+                results={gameStates.results.map(item => <p key={uuidv4()}>{item}</p>)}
             />
             <div className='board-container'>
                 <div className='board'>
@@ -141,8 +171,8 @@ export default function Board() {
                     <Row cells={cells[1]} handleClick={handleClick} />
                     <Row cells={cells[2]} handleClick={handleClick} />
                 </div>
-                {winner === '-' && <p className='winner'>There is no winner</p>}
-                {finalWinner !== '' && <p className='final-winner'>Final winner: {finalWinner}</p>}
+                {gameStates.winner === '-' && <p className='winner'>There is no winner</p>}
+                {gameStates.finalWinner !== '' && <p className='final-winner'>Final winner: {gameStates.finalWinner}</p>}
             </div>
         </>
     )
